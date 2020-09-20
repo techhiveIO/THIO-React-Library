3b4510e7c29d2f707d0d1841404960e5d5b69700
aliobaji <ali@techhive.io>
2020-09-20 14:39:31 +0300

    wip(app): scroll animation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,37 +2,33 @@ import React from 'react';
 
 import 'th-pattern-library/dist/index.css';
 import {
-  Button,
-  TrafficSign,
-  MainAction,
   FoldStepper
 } from 'th-pattern-library';
 
 const App = () => {
   return (
-    <div>
-      <Button label='Primary Button' />
-      <Button type='secondary' label='Secondary Button' />
-      <Button type='clear' label='Clear Button' />
-      <Button type='outline' label='Outline Button' />
-      <TrafficSign label='Traffic Signal' />
-      <TrafficSign label='Traffic Signal' type='clear' direction='left' />
-      <TrafficSign label='Traffic Signal' type='outline' direction='left' />
-      <MainAction actionLabel={'Start a Project'} />
+    <div className='container'>
 
-      <FoldStepper totalSteps={5} />
+      <div className='step step--blue'/>
+      <div className='step step--red'/>
+      <div className='step step--blue'/>
+      <div className='step step--red'/>
 
-      <form>
-        <div className={'form-field'}>
-          <label>Input Label</label>
-          <input
-            type='text'
-            placeholder='Placeholder'
-            name='text'
-            className={'is_error'}
-          />
-        </div>
-      </form>
+      <div className="foldStepper">
+        <FoldStepper totalSteps={5} />
+      </div>
+
+      {/*<form>*/}
+      {/*  <div className={'form-field'}>*/}
+      {/*    <label>Input Label</label>*/}
+      {/*    <input*/}
+      {/*      type='text'*/}
+      {/*      placeholder='Placeholder'*/}
+      {/*      name='text'*/}
+      {/*      className={'is_error'}*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*</form>*/}
     </div>
   );
 };
